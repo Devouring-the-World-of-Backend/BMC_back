7afed01d6fadd783bb105207eee63227c7b3c0bd
bmchun00 <bmchun00@korea.ac.kr>
2024-03-26 18:44:30 +0900

    feat: Book모델 정의

--- week2.py
+++ week2.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    published_year: int
+    
 app = FastAPI()
 
 @app.get("/")
