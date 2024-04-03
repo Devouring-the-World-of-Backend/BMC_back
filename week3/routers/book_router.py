c978cd492c07421f6eb22a28f642f4a3c01eee7b
bmchun00 <bmchun00@korea.ac.kr>
2024-04-03 15:05:13 +0900

    feat: CRUD 기능 구현

--- week3/routers/book_router.py
+++ week3/routers/book_router.py
@@ -10,13 +10,30 @@ router = APIRouter()
 @router.post("/books/", response_model=book.Book)
 def create_book(book: book.BookCreate):
     db = Session()
-    db_user = crud.create_book(db=db, book=book)
+    db_book = crud.create_book(db=db, book=book)
     db.close()
-    return db_user
+    return db_book
 
+# 모든 책을 불러옴
 @router.get("/books/", response_model=List[book.Book])
 def get_books():
     db = Session()
-    db_users = crud.get_books(db=db)
+    db_books = crud.get_books(db=db)
     db.close()
-    return db_users
+    return db_books
+
+# 특정 도서 업데이트
+@router.put("/books/{bid}", response_model=book.BookUpdate)
+def update_book(bid:int, book:book.BookUpdate):
+    db = Session()
+    db_book = crud.update_book(db=db, book=book, book_id = bid)
+    db.close()
+    return db_book
+
+# 특정 도서 삭제
+@router.delete("/books/{bid}", response_model=book.Book)
+def delete_book(bid:int):
+    db = Session()
+    db_book = crud.delete_book(db=db, book_id = bid)
+    db.close()
+    return db_book
